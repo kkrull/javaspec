284f1cf43fc9a0059a6c23870c25c5c1c64b0ed8
Kyle Krull <kkrull@indigobio.com>
2014-07-25 09:28:16 -0500

    Faked out scenario to make sure it's working.

--- src/test/java/org/jspec/JSpecRunnerSteps.java
+++ src/test/java/org/jspec/JSpecRunnerSteps.java
@@ -1,9 +1,10 @@
 package org.jspec;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -25,6 +26,15 @@ public final class JSpecRunnerSteps {
 
 	@Then("^the test runner should run all the tests in the class$")
 	public void the_test_runner_should_run_all_the_tests_in_the_class() throws Throwable {
-		assertEquals(42, this.result.getRunCount());
+		//Can't simply count tests, because the cucumber tests count too
+		for(Failure f : this.result.getFailures()) {
+			Throwable ex = f.getException();
+			System.out.printf("Looking at a test that failed with %s: %s\n", ex.getClass().getName(), ex);
+			if(JSpecTests.TestRanException.class.equals(ex.getClass())) {
+				return;
+			}
+		}
+	
+		fail("JSpec test was not run, or did not fail as expected");
 	}
 }
--- src/test/java/org/jspec/JSpecTests.java
+++ src/test/java/org/jspec/JSpecTests.java
@@ -1,4 +1,12 @@
 package org.jspec;
 
-public final class JSpecTests {
+import org.junit.Test;
+
+public class JSpecTests {
+	@Test
+	public void nativeJUnitTest() {	throw new TestRanException();	}
+	
+	@SuppressWarnings("serial")
+	// Nothing to version; only used in 1 place
+	public final class TestRanException extends RuntimeException {}
 }
