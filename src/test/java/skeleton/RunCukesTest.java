df977f57fa8c35c1ae3234b081e3a5b50a0057ea
Kyle Krull <kkrull@indigobio.com>
2014-07-24 23:12:21 -0500

    Color output

--- src/test/java/skeleton/RunCukesTest.java
+++ src/test/java/skeleton/RunCukesTest.java
@@ -5,6 +5,6 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(monochrome = true, format = {"pretty", "html:target/cucumber", "rerun:target/rerun.txt"})
+@CucumberOptions(monochrome = false, format = {"pretty", "html:target/cucumber", "rerun:target/rerun.txt"})
 public class RunCukesTest {
 }
