028b159b74727b247f257ce2203e6b3cd084fa73
Kyle Krull <kkrull@indigobio.com>
2014-07-28 18:51:25 -0500

    Using @RunWith(Enclosed.class) to nest tests This makes it easier to identify and isolate the tests for each method from the others. It works as long as the nested classes are static classes.  Using @RunWith(Enclosed.class) checks for this, but also seems to have the side effect of showing a doubled ignored test for each pass/fail test. Removing @RunWith(Enclosed.class) still runs the tests as a whole suite, but only allows you to run one nested class at a time in Eclipse.

--- src/test/java/org/jspec/JSpecRunnerTests.java
+++ src/test/java/org/jspec/JSpecRunnerTests.java
@@ -3,31 +3,63 @@ package org.jspec;
 import static org.junit.Assert.*;
 
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.jspec.dsl.It;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.Description;
-import org.junit.runner.notification.RunNotifier;
+import org.junit.runner.RunWith;
 import org.junit.runners.model.InitializationError;
 
 import com.google.common.collect.Lists;
 
+@RunWith(Enclosed.class)
 public class JSpecRunnerTests {
-
-  protected final List<String> testSequence = new LinkedList<String>();
-  
-  static Description descriptionOf(Class<?> testClass) {
-    JSpecRunner runner = runnerFor(testClass);
-    return runner.getDescription();
+  public static class getDescription {
+    static Description descriptionOf(Class<?> testClass) {
+      JSpecRunner runner = runnerFor(testClass);
+      return runner.getDescription();
+    }
+    
+    @Test
+    public void givenAClass_hasTheGivenTestClass() {
+      assertEquals(NoTests.class, descriptionOf(NoTests.class).getTestClass());
+    }
+    
+    @Test
+    public void givenAClassWithoutAnnotations_hasEmptyAnnotations() {
+      assertEquals(Collections.emptyList(), descriptionOf(NoTests.class).getAnnotations());
+    }
+    
+    @Test
+    public void givenAClassWithAnnotations_hasThoseAnnotations() {
+      assertNotNull(descriptionOf(IgnoredTests.class).getAnnotation(Ignore.class));
+    }
+    
+    @Test
+    public void givenAClassWithNoItFields_hasNoChildren() {
+      assertEquals(Collections.emptyList(), descriptionOf(NoTests.class).getChildren());
+    }
+    
+    @Test
+    public void givenAClassWith1OrMoreItFields_hasAChildForEach() {
+      List<Description> children = descriptionOf(TwoTests.class).getChildren();
+      List<String> names = children.stream().map(Description::getDisplayName).sorted().collect(Collectors.toList());
+      assertEquals(Lists.newArrayList(
+        "first_test(org.jspec.JSpecRunnerTests$TwoTests)",
+        "second_test(org.jspec.JSpecRunnerTests$TwoTests)"),
+        names);
+    }
   }
   
-  static void runTests(Class<?> testClass) {
-    JSpecRunner runner = runnerFor(testClass);
-    runner.run(new RunNotifier());
+  public static class getChildren {
+    @Test
+    public void works() {
+      fail("pending");
+    }
   }
   
   static JSpecRunner runnerFor(Class<?> testClass) {
@@ -38,7 +70,7 @@ public class JSpecRunnerTests {
     }
   }
   
-  @Ignore final class IgnoredTests {}
+  @Ignore class IgnoredTests {}
 
   class NoTests {
     public NoTests() {}
@@ -48,44 +80,5 @@ public class JSpecRunnerTests {
     public TwoTests() {}
     It first_test = () -> assertEquals(1, 1);
     It second_test = () -> assertEquals(2, 2);
-  }
-  
-  /* getDescription */
-
-  @Test
-  public void getDescription_hasTheGivenTestClass() {
-    assertEquals(NoTests.class, descriptionOf(NoTests.class).getTestClass());
-  }
-  
-  @Test
-  public void getDescription_givenAClassWithoutAnnotations_hasEmptyAnnotations() {
-    assertEquals(Collections.emptyList(), descriptionOf(NoTests.class).getAnnotations());
-  }
-  
-  @Test
-  public void getDescription_givenAClassWithAnnotations_hasThoseAnnotations() {
-    assertNotNull(descriptionOf(IgnoredTests.class).getAnnotation(Ignore.class));
-  }
-  
-  @Test
-  public void getDescription_givenAClassWithNoItFields_hasNoChildren() {
-    assertEquals(Collections.emptyList(), descriptionOf(NoTests.class).getChildren());
-  }
-  
-  @Test
-  public void getDescription_givenAClassWith1OrMoreItFields_hasAChildForEach() {
-    List<Description> children = descriptionOf(TwoTests.class).getChildren();
-    List<String> names = children.stream().map(Description::getDisplayName).sorted().collect(Collectors.toList());
-    assertEquals(Lists.newArrayList(
-      "first_test(org.jspec.JSpecRunnerTests$TwoTests)",
-      "second_test(org.jspec.JSpecRunnerTests$TwoTests)"),
-      names);
-  }
-  
-  /* getChildren */
-  
-  @Test
-  public void getChildren_works() {
-    fail("pending");
   }
 }
--- src/test/java/org/jspec/ReflectionUtilTests.java
+++ src/test/java/org/jspec/ReflectionUtilTests.java
@@ -1,6 +1,7 @@
 package org.jspec;
 
 import static org.junit.Assert.*;
+
 import static org.junit.Assume.*;
 
 import java.lang.reflect.Field;
@@ -10,68 +11,72 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
 
+@RunWith(Enclosed.class)
 public class ReflectionUtilTests {
-
-  @Test
-  public void fieldsOfType_givenClassWithNoMatchingFields_returnsEmptyList() {
-    assertFieldsOfType(MatchingType.class, EmptyClass.class);
-    assertFieldsOfType(MatchingType.class, EmptyNestedClass.class);
-  }
-  
-  @Test
-  public void fieldsOfType_givenClassWith1OrMoreMatchingFields_returnsEachField() {
-    assertFieldsOfType(MatchingType.class, MixedFields.class, "matching");
-  }
-  
-  @Test
-  public void fieldsOfType_givenAnyFieldVisibility_returnsTheField() {
-    assertFieldsOfType(MatchingType.class, FieldVisibility.class,
-      "publicField", "protectedField", "defaultVisibilityField", "privateField");
-  }
-  
-  @Test
-  public void fieldsOfType_givenAClassWithInheritedFields_excludesThoseFields() {
-    MatchingType value = new MatchingType();
-    assumeTrue("The super type's field should be inherited", new SubType(value).superTypeField == value);
-    assertFieldsOfType(MatchingType.class, SubType.class, "subTypeField");
-  }
-  
-  static void assertFieldsOfType(Class<?> fieldType, Class<?> typeToInspect, String... names) {
-    final Comparator<String> alphabetical = (x, y) -> x.compareTo(y);
-    List<Field> fields = ReflectionUtil.fieldsOfType(fieldType, typeToInspect);
-    List<String> actualNames = fields.stream().map(Field::getName).sorted(alphabetical).collect(Collectors.toList());
-    List<String> expectedNames = Arrays.asList(names).stream().sorted(alphabetical).collect(Collectors.toList());
-    assertEquals(expectedNames, actualNames);
-  }
-  
-  class EmptyNestedClass { /* Not devoid of fields; there is 1 pointing to the containing class */ }
-  
-  class MixedFields {
-    public MatchingType matching;
-    public NonMatchingType nonMatching;
-  }
-  
-  class FieldVisibility {
-    public MatchingType publicField;
-    protected MatchingType protectedField;
-    MatchingType defaultVisibilityField;
-    
-    @SuppressWarnings("unused")
-    private MatchingType privateField;
-  }
-  
-  class SubType extends SuperType {
-    public MatchingType subTypeField;
-    public SubType(MatchingType superTypeField) {
-      this.superTypeField = superTypeField;
+  public static class fieldsOfType {
+    @Test
+    public void givenClassWithNoMatchingFields_returnsEmptyList() {
+      assertFieldsOfType(MatchingType.class, EmptyClass.class);
+      assertFieldsOfType(MatchingType.class, EmptyNestedClass.class);
     }
+    
+    @Test
+    public void givenClassWith1OrMoreMatchingFields_returnsEachField() {
+      assertFieldsOfType(MatchingType.class, MixedFields.class, "matching");
+    }
+    
+    @Test
+    public void givenAnyFieldVisibility_returnsTheField() {
+      assertFieldsOfType(MatchingType.class, FieldVisibility.class,
+        "publicField", "protectedField", "defaultVisibilityField", "privateField");
+    }
+    
+    @Test
+    public void givenAClassWithInheritedFields_excludesThoseFields() {
+      MatchingType value = new MatchingType();
+      assumeTrue("The super type's field should be inherited", new SubType(value).superTypeField == value);
+      assertFieldsOfType(MatchingType.class, SubType.class, "subTypeField");
+    }
+    
+    static void assertFieldsOfType(Class<?> fieldType, Class<?> typeToInspect, String... names) {
+      final Comparator<String> alphabetical = (x, y) -> x.compareTo(y);
+      List<Field> fields = ReflectionUtil.fieldsOfType(fieldType, typeToInspect);
+      List<String> actualNames = fields.stream().map(Field::getName).sorted(alphabetical).collect(Collectors.toList());
+      List<String> expectedNames = Arrays.asList(names).stream().sorted(alphabetical).collect(Collectors.toList());
+      assertEquals(expectedNames, actualNames);
+    }
+    
+    class EmptyNestedClass { /* Not devoid of fields; there is 1 pointing to the containing class */ }
+    
+    class MixedFields {
+      public MatchingType matching;
+      public NonMatchingType nonMatching;
+    }
+    
+    class FieldVisibility {
+      public MatchingType publicField;
+      protected MatchingType protectedField;
+      MatchingType defaultVisibilityField;
+      
+      @SuppressWarnings("unused")
+      private MatchingType privateField;
+    }
+    
+    class SubType extends SuperType {
+      public MatchingType subTypeField;
+      public SubType(MatchingType superTypeField) {
+        this.superTypeField = superTypeField;
+      }
+    }
+    
+    class SuperType {
+      public MatchingType superTypeField;
+    }
+    
+    class MatchingType {}
+    class NonMatchingType {}
   }
-  
-  class SuperType {
-    public MatchingType superTypeField;
-  }
-  
-  class MatchingType {}
-  class NonMatchingType {}
 }
