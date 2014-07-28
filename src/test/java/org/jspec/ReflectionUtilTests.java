1c92cb2ad153f10e739f0dbd6b4b9de5d97b60e1
Kyle Krull <kkrull@indigobio.com>
2014-07-28 15:16:55 -0500

    Making sure it can access each field visibility.

--- src/test/java/org/jspec/ReflectionUtilTests.java
+++ src/test/java/org/jspec/ReflectionUtilTests.java
@@ -3,30 +3,38 @@ package org.jspec;
 import static org.junit.Assert.*;
 
 import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.Test;
-
-import com.google.common.collect.Lists;
 
 public class ReflectionUtilTests {
 
   @Test
   public void fieldsOfType_givenClassWithNoMatchingFields_returnsEmptyList() {
-    assertEquals(Lists.newArrayList(), ReflectionUtil.fieldsOfType(MatchingType.class, EmptyClass.class));
-    assertEquals(Lists.newArrayList(), ReflectionUtil.fieldsOfType(MatchingType.class, EmptyNestedClass.class));
+    assertFieldsOfType(MatchingType.class, EmptyClass.class);
+    assertFieldsOfType(MatchingType.class, EmptyNestedClass.class);
   }
   
   @Test
   public void fieldsOfType_givenClassWith1OrMoreMatchingFields_returnsEachField() {
-    List<Field> returned = ReflectionUtil.fieldsOfType(MatchingType.class, MixedFields.class);
-    List<String> returnedNames = Lists.transform(returned, Field::getName);
-    assertEquals(Lists.newArrayList("matching"), returnedNames);
+    assertFieldsOfType(MatchingType.class, MixedFields.class, "matching");
   }
   
   @Test
-  public void fieldsOfType_givenMatchingDefaultVisibilityField_returnsMatchingField() {
-    fail("pending");
+  public void fieldsOfType_givenAnyFieldVisibility_returnsTheField() {
+    assertFieldsOfType(MatchingType.class, FieldVisibility.class,
+      "publicField", "protectedField", "defaultVisibilityField", "privateField");
+  }
+  
+  static void assertFieldsOfType(Class<?> fieldType, Class<?> typeToInspect, String... names) {
+    final Comparator<String> alphabetical = (x, y) -> x.compareTo(y);
+    List<Field> fields = ReflectionUtil.fieldsOfType(fieldType, typeToInspect);
+    List<String> actualNames = fields.stream().map(Field::getName).sorted(alphabetical).collect(Collectors.toList());
+    List<String> expectedNames = Arrays.asList(names).stream().sorted(alphabetical).collect(Collectors.toList());
+    assertEquals(expectedNames, actualNames);
   }
   
   class EmptyNestedClass { /* Not devoid of fields; there is 1 pointing to the containing class */ }
@@ -34,6 +42,15 @@ public class ReflectionUtilTests {
   class MixedFields {
     public MatchingType matching;
     public NonMatchingType nonMatching;
+  }
+  
+  class FieldVisibility {
+    public MatchingType publicField;
+    protected MatchingType protectedField;
+    MatchingType defaultVisibilityField;
+    
+    @SuppressWarnings("unused")
+    private MatchingType privateField;
   }
   
   class MatchingType {}
