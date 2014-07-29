db8859568b1fda4e17db3aa53cd7c9450bd246d7
Kyle Krull <kkrull@indigobio.com>
2014-07-28 19:08:43 -0500

    For further nesting levels, the Enclosed test runner seems to miss some tests when running in Eclipse.

--- src/test/java/org/jspec/JSpecRunnerTests.java
+++ src/test/java/org/jspec/JSpecRunnerTests.java
@@ -16,52 +16,63 @@ import org.junit.runners.model.InitializationError;
 
 import com.google.common.collect.Lists;
 
-@RunWith(Enclosed.class)
+//@RunWith(Enclosed.class)
 public class JSpecRunnerTests {
   public static class getDescription {
-    static Description descriptionOf(Class<?> testClass) {
-      JSpecRunner runner = runnerFor(testClass);
-      return runner.getDescription();
-    }
-    
     @Test
     public void givenAClass_hasTheGivenTestClass() {
       assertEquals(NoTests.class, descriptionOf(NoTests.class).getTestClass());
     }
-    
+
     @Test
     public void givenAClassWithoutAnnotations_hasEmptyAnnotations() {
       assertEquals(Collections.emptyList(), descriptionOf(NoTests.class).getAnnotations());
     }
-    
+
     @Test
     public void givenAClassWithAnnotations_hasThoseAnnotations() {
       assertNotNull(descriptionOf(IgnoredTests.class).getAnnotation(Ignore.class));
     }
-    
-    @Test
-    public void givenAClassWithNoItFields_hasNoChildren() {
-      assertEquals(Collections.emptyList(), descriptionOf(NoTests.class).getChildren());
+
+    public static class givenAClassWithNoItFields {
+      Description description = descriptionOf(NoTests.class);
+
+      @Test
+      public void hasNoChildren() {
+        assertEquals(Collections.emptyList(), description.getChildren());
+      }
+
+      @Test
+      public void hasTestCount0() {
+        assertEquals(0, description.testCount());
+      }
     }
-    
-    @Test
-    public void givenAClassWith1OrMoreItFields_hasAChildForEach() {
-      List<Description> children = descriptionOf(TwoTests.class).getChildren();
-      List<String> names = children.stream().map(Description::getDisplayName).sorted().collect(Collectors.toList());
-      assertEquals(Lists.newArrayList(
-        "first_test(org.jspec.JSpecRunnerTests$TwoTests)",
-        "second_test(org.jspec.JSpecRunnerTests$TwoTests)"),
-        names);
+
+    public static class givenAClassWith1OrMoreItFields {
+      Description description = descriptionOf(TwoTests.class);
+
+      @Test
+      public void hasAChildForEach() {
+        List<Description> children = description.getChildren();
+        List<String> names = children.stream().map(Description::getDisplayName).sorted().collect(Collectors.toList());
+        assertEquals(Lists.newArrayList(
+          "first_test(org.jspec.JSpecRunnerTests$TwoTests)",
+          "second_test(org.jspec.JSpecRunnerTests$TwoTests)"),
+          names);
+      }
+
+      @Test
+      public void hasTestCountForEachField() {
+        assertEquals(2, description.testCount());
+      }
+    }
+
+    static Description descriptionOf(Class<?> testClass) {
+      JSpecRunner runner = runnerFor(testClass);
+      return runner.getDescription();
     }
   }
-  
-  public static class getChildren {
-    @Test
-    public void works() {
-      fail("pending");
-    }
-  }
-  
+
   static JSpecRunner runnerFor(Class<?> testClass) {
     try {
       return new JSpecRunner(testClass);
@@ -69,15 +80,13 @@ public class JSpecRunnerTests {
       throw new RuntimeException(e);
     }
   }
-  
-  @Ignore class IgnoredTests {}
 
-  class NoTests {
-    public NoTests() {}
-  }
-  
+  @Ignore
+  class IgnoredTests {}
+
+  class NoTests {}
+
   class TwoTests {
-    public TwoTests() {}
     It first_test = () -> assertEquals(1, 1);
     It second_test = () -> assertEquals(2, 2);
   }
