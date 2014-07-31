7c69e81afacfc9460c1fc380783265ecaee7564d
Kyle Krull <kkrull@indigobio.com>
2014-07-31 17:29:23 -0500

    Maven cucumber tests, again obeying surefire naming conventions

--- src/test/java/FeatureTest.java
+++ src/test/java/FeatureTest.java
@@ -4,4 +4,4 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(format = { "pretty", "html:target/cucumber", "rerun:target/rerun.txt" }, monochrome = true)
-public final class CucumberRunner {}
+public final class FeatureTest {}
