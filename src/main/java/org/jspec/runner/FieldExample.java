f9a64bf3f0d8dba2b7a3a4bdb42aba8c5b2e72ea
Kyle Krull <kkrull@indigobio.com>
2014-08-07 18:09:21 -0500

    Constructing test instances.

--- src/main/java/org/jspec/runner/FieldExample.java
+++ src/main/java/org/jspec/runner/FieldExample.java
@@ -2,6 +2,7 @@ package org.jspec.runner;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.util.stream.Stream;
 
 import org.jspec.dsl.It;
 
@@ -9,6 +10,9 @@ final class FieldExample implements Example {
   private final Field behavior;
   
   public FieldExample(Field behavior) {
+    if(!It.class.equals(behavior.getType()))
+      throw new UnsupportedFieldException(behavior);
+    
     this.behavior = behavior;
   }
   
@@ -22,32 +26,38 @@ final class FieldExample implements Example {
     Object context;
     try {
       Class<?> contextClass = behavior.getDeclaringClass();
-      Constructor<?> noArgConstructor = contextClass.getConstructors()[0];
-      noArgConstructor.setAccessible(true);
-      context = noArgConstructor.newInstance();
+      Constructor<?> noArgConstructor = contextClass.getConstructor();
+//      context = noArgConstructor.newInstance();
     } catch (Exception e) {
-      e.printStackTrace();
-      throw new UnsupportedOperationException(e);
+      throw new UnsupportedConstructorException(behavior.getDeclaringClass());
     }
     
-    It thunk;
-    try {
-      behavior.setAccessible(true);
-      thunk = (It)behavior.get(context);
-    } catch (Exception e) {
-      throw new UnsupportedFieldException(behavior);
-    }
-    
-    try {
-      thunk.run();
-    } catch (Exception e) {
-      e.printStackTrace();
-      throw new UnsupportedOperationException(e);
+//    It thunk;
+//    try {
+//      behavior.setAccessible(true);
+//      thunk = (It)behavior.get(context);
+//    } catch (Exception e) {
+//      throw new UnsupportedFieldException(behavior);
+//    }
+//    
+//    try {
+//      thunk.run();
+//    } catch (Exception e) {
+//      e.printStackTrace();
+//      throw new UnsupportedOperationException(e);
+//    }
+  }
+  
+  public static final class UnsupportedConstructorException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public UnsupportedConstructorException(Class<?> context) {
+      super(String.format("Unable to find a no-argument constructor for class %s", context.getName()));
     }
   }
   
   public static final class UnsupportedFieldException extends RuntimeException {
-    private static final long serialVersionUID = 4899732341344955353L;
+    private static final long serialVersionUID = 1L;
 
     public UnsupportedFieldException(Field f) {
       super(String.format("Invalid type for %s.%s: %s", 
--- src/test/java/org/jspec/runner/FieldExampleTest.java
+++ src/test/java/org/jspec/runner/FieldExampleTest.java
@@ -4,15 +4,19 @@ import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.is;
 import static org.jspec.util.Assertions.assertThrows;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 
+import java.lang.reflect.Field;
 import java.util.LinkedList;
 import java.util.List;
 
 import org.jspec.dsl.It;
 import org.jspec.proto.JSpecExamples;
+import org.jspec.runner.FieldExample.UnsupportedConstructorException;
 import org.jspec.runner.FieldExample.UnsupportedFieldException;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -20,6 +24,24 @@ import de.bechte.junit.runners.context.HierarchicalContextRunner;
 
 @RunWith(HierarchicalContextRunner.class)
 public class FieldExampleTest {
+  public class constructor {
+    public class givenABehaviorFieldOfSomeOtherType {
+      private final Class<?> contextClass = UnsupportedBehaviorField.class;
+      private final Field field;
+      
+      public givenABehaviorFieldOfSomeOtherType() throws Exception {
+        this.field = contextClass.getDeclaredField("notAnItField");
+      }
+      
+      @Test
+      public void throwsUnsupportedFieldException() {
+        assertThrows(UnsupportedFieldException.class, 
+          String.format("Invalid type for %s.notAnItField: java.lang.Integer", contextClass.getName()),
+          () -> new FieldExample(field));
+      }
+    }
+  }
+  
   public class describeBehavior {
     private final Example subject;
     It describesADesiredBehavior;
@@ -35,34 +57,35 @@ public class FieldExampleTest {
   }
   
   public class run {
-    public class givenABehaviorFieldOfSomeOtherType {
-      private final Class<?> contextClass = UnsupportedBehaviorField.class;
-      private final Example subject;
-      
-      public givenABehaviorFieldOfSomeOtherType() throws Exception {
-        this.subject = new FieldExample(contextClass.getDeclaredField("notAnItField"));
+    protected final List<String> events = new LinkedList<String>();
+    
+    public class givenAClassWithoutACallableNoArgConstructor {
+      @Test
+      public void ThrowsUnsupportedConstructorException() throws Exception  {
+        assertThrowsUnsupportedConstructorException(JSpecExamples.HiddenConstructor.class, "is_otherwise_valid");
       }
       
+      private void assertThrowsUnsupportedConstructorException(Class<?> context, String itFieldName) throws Exception {
+        Field field = context.getDeclaredField(itFieldName);
+        Example subject = new FieldExample(field);
+        assertThrows(UnsupportedConstructorException.class,
+          "Unable to find a no-argument constructor for class org.jspec.proto.JSpecExamples$HiddenConstructor",
+          () -> subject.run());
+      }
+    }
+    
+    public class givenAFaultyConstructor {
       @Test
-      public void throwsUnsupportedFieldException() {
-        assertThrows(UnsupportedFieldException.class, 
-          String.format("Invalid type for %s.notAnItField: java.lang.Integer", contextClass.getName()),
-          subject::run);
+      public void failsTheTestByThrowingTestSetupFailedException() {
+        fail("pending");
       }
     }
     
     public class givenAnItField {
-      private final Example subject;
-      private final List<String> events = new LinkedList<String>();
-      
-      public givenAnItField() throws Exception {
-        this.subject = new FieldExample(JSpecExamples.One.class.getDeclaredField("only_test"));
-      }
-      
       @Before
       public void spy() {
         JSpecExamples.One.setEventListener(events::add);
-        this.subject.run();
+//        this.subject.run();
       }
       
       @After
@@ -70,7 +93,7 @@ public class FieldExampleTest {
         JSpecExamples.One.setEventListener(null);
       }
       
-      @Test
+      @Test @Ignore("wip")
       public void constructsTheContextClassThenRunsTheFunctionAssignedToTheGivenField() {
         assertThat(events, contains("JSpecExamples.One::new", "JSpecExamples.One::only_test"));
       }
@@ -78,6 +101,6 @@ public class FieldExampleTest {
   }
   
   public static class UnsupportedBehaviorField {
-    Integer notAnItField = 42;
+    Integer notAnItField;
   }
 }
