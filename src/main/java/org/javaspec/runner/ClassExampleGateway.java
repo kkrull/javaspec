998fb6a1a643e6830e4599b0372673ba999a8ded
Kyle Krull <kkrull@indigobio.com>
2014-08-25 13:07:20 -0500

    ClassExampleGateway.findInitializationErrors

--- src/main/java/org/javaspec/runner/ClassExampleGateway.java
+++ src/main/java/org/javaspec/runner/ClassExampleGateway.java
@@ -1,23 +1,37 @@
 package org.javaspec.runner;
 
-import java.util.Collections;
+import static java.util.stream.Collectors.toList;
+
+import java.lang.reflect.Field;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Stream;
 
-final class ClassExampleGateway implements ExampleGateway {
-  ClassExampleGateway(Class<?> contextClass) {
-//    this.contextClass = contextClass;
-  }
+import org.javaspec.dsl.Because;
+import org.javaspec.dsl.Cleanup;
+import org.javaspec.dsl.Establish;
+import org.javaspec.dsl.It;
+import org.javaspec.util.DfsSearch;
 
+final class ClassExampleGateway implements ExampleGateway {
+  private final Class<?> contextClass;
+  
+  ClassExampleGateway(Class<?> contextClass) {
+    this.contextClass = contextClass;
+  }
+  
   @Override
   public List<Throwable> findInitializationErrors() {
-    return Collections.emptyList();
-//    throw new UnsupportedOperationException();
-  }
-
-  @Override
-  public Stream<NewExample> getExamples() {
-    throw new UnsupportedOperationException();
+    List<Throwable> list = new LinkedList<Throwable>();
+    if(!hasAnyTests())
+      list.add(new NoExamplesException(contextClass));
+    if(isStepSequenceAmbiguous(Establish.class))
+      list.add(new UnknownStepExecutionSequenceException(contextClass, Establish.class.getSimpleName()));
+    if(isStepSequenceAmbiguous(Because.class))
+      list.add(new UnknownStepExecutionSequenceException(contextClass, Because.class.getSimpleName()));
+    if(isStepSequenceAmbiguous(Cleanup.class))
+      list.add(new UnknownStepExecutionSequenceException(contextClass, Cleanup.class.getSimpleName()));
+    return list;
   }
 
   @Override
@@ -26,7 +40,43 @@ final class ClassExampleGateway implements ExampleGateway {
   }
 
   @Override
+  public Stream<NewExample> getExamples() {
+    throw new UnsupportedOperationException();
+  }
+  
+  @Override
   public Context getRootContext() {
     throw new UnsupportedOperationException();
+  }
+  
+  private boolean hasAnyTests() {
+    DfsSearch<Class<?>> search = new DfsSearch<Class<?>>(contextClass, parent -> parent.getDeclaredClasses());
+    return search.anyNodeMatches(x -> ReflectionUtil.hasFieldsOfType(It.class, x));
+  }
+  
+  private boolean isStepSequenceAmbiguous(Class<?> typeOfStep) {
+    //No guarantee that reflection will sort fields by order of declaration; running them out of order could fail
+    List<Field> thereCanBeOnlyOne = ReflectionUtil.fieldsOfType(typeOfStep, contextClass).collect(toList());
+    return thereCanBeOnlyOne.size() > 1; 
+  }
+  
+//  private Field onlyFieldOrNull(Class<?> typeOfField) {
+//    List<Field> matchingFields = ReflectionUtil.fieldsOfType(typeOfField, contextClass).collect(toList());
+//    return matchingFields.isEmpty() ? null : matchingFields.get(0);
+//  }
+  
+  public static class UnknownStepExecutionSequenceException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+    public UnknownStepExecutionSequenceException(Class<?> contextClass, String whatStepIsAmbiguous) {
+      super(String.format("Impossible to determine running order of multiple %s functions in test context %s",
+        whatStepIsAmbiguous, contextClass.getName()));
+    }
+  }
+  
+  public static class NoExamplesException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+    public NoExamplesException(Class<?> contextClass) {
+      super(String.format("Test context %s must contain at least 1 example in an It field", contextClass.getName()));
+    }
   }
 }
--- src/test/java/org/javaspec/runner/ClassExampleGatewayTest.java
+++ src/test/java/org/javaspec/runner/ClassExampleGatewayTest.java
@@ -1,5 +1,16 @@
 package org.javaspec.runner;
 
+import static java.util.stream.Collectors.toList;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+import java.util.Collections;
+
+import org.javaspec.proto.ContextClasses;
+import org.javaspec.runner.ClassExampleGateway.NoExamplesException;
+import org.javaspec.runner.ClassExampleGateway.UnknownStepExecutionSequenceException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -8,9 +19,53 @@ import de.bechte.junit.runners.context.HierarchicalContextRunner;
 @RunWith(HierarchicalContextRunner.class)
 public class ClassExampleGatewayTest {
   public class findInitializationErrors {
-    @Test
-    public void runs() {
+    public class givenAClassWith0OrMoreInnerClasses {
+      @Test
+      public void givenAClassWith2OrMoreEstablishFields_containsUnknownStepExecutionSequenceException() {
+        shouldFindInitializationError(ContextClasses.TwoEstablish.class, UnknownStepExecutionSequenceException.class,
+          "Impossible to determine running order of multiple Establish functions in test context org.javaspec.proto.ContextClasses$TwoEstablish");
+      }
       
+      @Test
+      public void givenAClassWith2OrMoreBecauseFields_containsUnknownStepExecutionSequenceException() {
+        shouldFindInitializationError(ContextClasses.TwoBecause.class, UnknownStepExecutionSequenceException.class,
+          "Impossible to determine running order of multiple Because functions in test context org.javaspec.proto.ContextClasses$TwoBecause");
+      }
+      
+      @Test
+      public void givenAClassWith2OrMoreCleanupFields_containsUnknownStepExecutionSequenceException() {
+        shouldFindInitializationError(ContextClasses.TwoCleanup.class, UnknownStepExecutionSequenceException.class,
+          "Impossible to determine running order of multiple Cleanup functions in test context org.javaspec.proto.ContextClasses$TwoCleanup");
+      }
+      
+      @Test
+      public void givenNoClassesWith1OrMoreItFields_containsNoExamplesException() {
+        shouldFindInitializationError(ContextClasses.Empty.class, NoExamplesException.class,
+          "Test context org.javaspec.proto.ContextClasses$Empty must contain at least 1 example in an It field");
+      }
+      
+      class givenAtLeast1ItFieldSomewhere_andNoClassesWith2OrMoreOfTheSameFixture {
+        @Test
+        public void returnsEmptyList() {
+          ExampleGateway subject = new ClassExampleGateway(ContextClasses.Nested.class);
+          assertThat(subject.findInitializationErrors(), equalTo(Collections.emptyList()));
+        }
+      }
+    }
+    
+    private void shouldFindInitializationError(Class<?> contextType, Class<?> errorType, String errorMsg) {
+      ExampleGateway subject = new ClassExampleGateway(contextType);
+      assertThat(subject.findInitializationErrors().stream().map(x -> x.getClass()).collect(toList()),
+        contains(equalTo(errorType)));
+      assertThat(subject.findInitializationErrors().stream().map(Throwable::getMessage).collect(toList()),
+        contains(equalTo(errorMsg)));
+    }
+  }
+  
+  public class getRootContext {
+    @Test
+    public void doesSomething() {
+      fail("pending");
     }
   }
 }
