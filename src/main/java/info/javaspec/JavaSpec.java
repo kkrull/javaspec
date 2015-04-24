e07a747683426882dffaca73f109f9144b268a9f
Kyle Krull <kyle.krull@here.com>
2015-04-24 12:59:09 -0500

    Starting extract external dependencies for a simpler, stateless approach to injecting external dependencies.

--- src/main/java/info/javaspec/JavaSpec.java
+++ src/main/java/info/javaspec/JavaSpec.java
@@ -8,27 +8,25 @@ import java.io.PrintStream;
  * See JavaSpecRunner for details on running tests.
  */
 public final class JavaSpec {
-  private static final PrintStream DEFAULT_CONSOLE = System.out;
-  private static PrintStream _console = DEFAULT_CONSOLE;
-  
-  private static final ExitHandler DEFAULT_SYSTEM = System::exit;
-  private static ExitHandler _system = DEFAULT_SYSTEM;
-  
-  /* Environment */
-  
-  public static void setEnvironment() {
-    setEnvironment(DEFAULT_CONSOLE, DEFAULT_SYSTEM);
-  }
-  
-  public static void setEnvironment(PrintStream console, ExitHandler system) {
-    _console = console;
-    _system = system;
-  }
+  private final PrintStream console;
+  private final ExitHandler system;
+  private final AppConfigGateway configGateway;
 
   /* Command line interface */
   
   public static void main(String... args) {
-    new JavaSpec().run(args);
+    main(System.out, System::exit, args);
+  }
+
+  public static void main(PrintStream console, ExitHandler system, String... args) {
+    JavaSpec cli = new JavaSpec(console, system);
+    cli.run(args);
+  }
+
+  public JavaSpec(PrintStream console, ExitHandler system) {
+    this.console = console;
+    this.system = system;
+    this.configGateway = new AppConfigGateway();
   }
 
   public void run(String... args) {
@@ -40,25 +38,25 @@ public final class JavaSpec {
       printUsage(1);
   }
 
-  private boolean isHelpCommand(String... args) {
+  private static boolean isHelpCommand(String... args) {
     return args.length == 0 || (args.length == 1 && "--help".equals(args[0]));
   }
 
   private void printUsage(int exitCode) {
-    _console.println(String.format("Usage: java %s --version", getClass().getName()));
-    _console.println("--help: Show this help");
-    _console.println("--version: Show the version");
-    _system.exit(exitCode);
+    console.println(String.format("Usage: java %s --version", getClass().getName()));
+    console.println("--help: Show this help");
+    console.println("--version: Show the version");
+    system.exit(exitCode);
   }
 
-  private boolean isVersionCommand(String... args) {
+  private static boolean isVersionCommand(String... args) {
     return args.length == 1 && "--version".equals(args[0]);
   }
 
   private void printVersion() {
-    String version = new AppConfigGateway().version();
-    _console.println(version);
-    _system.exit(0);
+    String version = configGateway.version();
+    console.println(version);
+    system.exit(0);
   }
 
   @FunctionalInterface
