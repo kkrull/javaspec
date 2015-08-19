d758cc91fd2de2c013b454c6b0d6aa3116114a2d
Kyle Krull <kyle.krull@here.com>
2015-08-19 08:53:06 -0500

    Simplified SpecFactory

--- src/main/java/info/javaspec/spec/SpecFactory.java
+++ src/main/java/info/javaspec/spec/SpecFactory.java
@@ -5,22 +5,12 @@ import org.junit.runner.Description;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
-import java.util.List;
 
 public class SpecFactory {
   public static FieldSpec create(Context context, Field it) {
-    return create(
-      context.getId(),
-      context.getDescription().getClassName(),
-      it,
-      new ArrayList<>(0),
-      new ArrayList<>(0));
-  }
-
-  private static FieldSpec create(String contextId, String descriptionClassName, Field it, List<Field> befores, List<Field> afters) {
-    String id = String.format("%s#%s", contextId, it.getName());
-    Description testDescription = Description.createTestDescription(descriptionClassName, humanize(it.getName()), id);
-    return new FieldSpec(id, testDescription, it, befores, afters);
+    String id = String.format("%s#%s", context.getId(), it.getName());
+    Description testDescription = Description.createTestDescription(context.getDescription().getClassName(), humanize(it.getName()), id);
+    return new FieldSpec(id, testDescription, it, new ArrayList<>(0), new ArrayList<>(0));
   }
 
   private static String humanize(String identifier) { return identifier.replace('_', ' '); }
