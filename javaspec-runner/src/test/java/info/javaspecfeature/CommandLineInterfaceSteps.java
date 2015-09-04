8e438f3cf671d21f664bc573446b4ce6aa94bbdf
Kyle Krull <kyle.krull@here.com>
2015-09-04 08:08:28 -0500

    Promoted version to 1.0

--- javaspec-runner/src/test/java/info/javaspecfeature/CommandLineInterfaceSteps.java
+++ javaspec-runner/src/test/java/info/javaspecfeature/CommandLineInterfaceSteps.java
@@ -44,7 +44,7 @@ public class CommandLineInterfaceSteps {
 
   @Then("^the command line interface should print its version number to the console$")
   public void the_command_line_interface_should_print_its_version_number_to_the_console() throws Exception {
-    Mockito.verify(console).println("1.0-SNAPSHOT");
+    Mockito.verify(console).println("1.0");
     Mockito.verifyNoMoreInteractions(console);
   }
 
