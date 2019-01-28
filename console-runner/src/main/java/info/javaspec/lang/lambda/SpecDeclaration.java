7d5a25c13aba4cb96b7f9ddba121148d8ca4df1a
Kyle Krull <kkrull@8thlight.com>
2019-01-28 10:27:14 -0600

    Refactor SpecDeclaration to singleton instance

--- console-runner/src/main/java/info/javaspec/lang/lambda/SpecDeclaration.java
+++ console-runner/src/main/java/info/javaspec/lang/lambda/SpecDeclaration.java
@@ -8,33 +8,53 @@ import java.util.Stack;
 
 /** Groups recently-declared specs into a suite of specs that can be run together */
 final class SpecDeclaration {
-  private static final Stack<SequentialSuite> _currentSuite = new Stack<>();
+  private static SpecDeclaration _instance;
+  private final Stack<SequentialSuite> currentSuite;
+
+  public static SpecDeclaration getInstance() {
+    return _instance;
+  }
 
   public static void beginDeclaration() {
-    if(!_currentSuite.isEmpty())
+    if(_instance != null)
       throw new IllegalStateException("Specs are already being declared");
 
-    _currentSuite.push(new SequentialSuite());
-  }
-
-  public static void declareSpecsFor(String subject, BehaviorDeclaration describeBehavior) {
-    SequentialSuite subjectSuite = new SequentialSuite(subject);
-    _currentSuite.peek().addChildSuite(subjectSuite); //Add the child suite in line with any other declared specs
-    _currentSuite.push(subjectSuite); //Push on to the stack in case there are nested describes
-    describeBehavior.declareSpecs();
-    _currentSuite.pop();
-  }
-
-  public static void createSpec(String intendedBehavior, BehaviorVerification verification) {
-    Spec spec = new DescriptiveSpec(intendedBehavior, verification);
-    _currentSuite.peek().addSpec(spec);
+    _instance = new SpecDeclaration(initRootSuite());
   }
 
   public static Suite endDeclaration() {
-    Suite suite = _currentSuite.pop();
-    if(!_currentSuite.isEmpty())
+    Suite suite = _instance.currentSuite.pop();
+    if(!_instance.currentSuite.isEmpty())
       throw new IllegalStateException("Spec declaration ended prematurely");
 
+    _instance = null;
     return suite;
+  }
+
+  private SpecDeclaration(Stack<SequentialSuite> currentSuite) {
+    this.currentSuite = currentSuite;
+  }
+
+  public void declareSpecsFor(String subject, BehaviorDeclaration describeBehavior) {
+    SequentialSuite subjectSuite = new SequentialSuite(subject);
+    currentSuite().addChildSuite(subjectSuite); //Add the child suite in line with any other declared specs
+    this.currentSuite.push(subjectSuite); //Push on to the stack in case there are nested describes
+    describeBehavior.declareSpecs();
+    this.currentSuite.pop();
+  }
+
+  public void createSpec(String intendedBehavior, BehaviorVerification verification) {
+    Spec spec = new DescriptiveSpec(intendedBehavior, verification);
+    currentSuite().addSpec(spec);
+  }
+
+  private static Stack<SequentialSuite> initRootSuite() {
+    Stack<SequentialSuite> currentSuite = new Stack<>();
+    currentSuite.push(new SequentialSuite());
+    return currentSuite;
+  }
+
+  private SequentialSuite currentSuite() {
+    return this.currentSuite.peek();
   }
 }
