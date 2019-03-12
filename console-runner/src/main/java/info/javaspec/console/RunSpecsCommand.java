f96c64d224028546689721f81e1cd9e5892c30bd
Kyle Krull <kkrull@8thlight.com>
2019-03-12 17:05:29 -0500

    Notes

--- console-runner/src/main/java/info/javaspec/console/RunSpecsCommand.java
+++ console-runner/src/main/java/info/javaspec/console/RunSpecsCommand.java
@@ -28,7 +28,7 @@ final class RunSpecsCommand implements Command {
     }
 
     SpecCollection rootCollection = this.finder.findSpecs(specClasses);
-    reporter.runStarting(); //TODO KDK: Move this to RootSpecCollection#run?
+    reporter.runStarting();
     rootCollection.runSpecs(reporter);
     reporter.runFinished();
     return reporter.hasFailingSpecs() ? 1 : 0;
