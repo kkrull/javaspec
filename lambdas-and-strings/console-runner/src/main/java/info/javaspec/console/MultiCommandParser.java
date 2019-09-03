11ab5f8495f352b60ba6cd6d8dbd0906d4a01c49
Kyle Krull <kkrull@8thlight.com>
2019-09-03 08:48:45 -0500

    Start parsing CLI commands

--- lambdas-and-strings/console-runner/src/main/java/info/javaspec/console/MultiCommandParser.java
+++ lambdas-and-strings/console-runner/src/main/java/info/javaspec/console/MultiCommandParser.java
@@ -2,22 +2,35 @@ package info.javaspec.console;
 
 import com.beust.jcommander.JCommander;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MultiCommandParser implements Main.ArgumentParser {
+  private final JCommander.Builder jCommanderBuilder;
   private final JCommanderArguments main;
+  private final Map<String, JCommanderArguments> commandArgs;
 
   public MultiCommandParser(JCommanderArguments main) {
     this.main = main;
+    this.commandArgs = new LinkedHashMap<>();
+    this.jCommanderBuilder = JCommander.newBuilder()
+      .addObject(this.main);
+  }
+
+  public void addCliCommand(String command, JCommanderArguments jCommanderArgs) {
+    this.commandArgs.put(command, jCommanderArgs);
+    this.jCommanderBuilder.addCommand(command, jCommanderArgs);
   }
 
   @Override
   public Command parseCommand(List<String> arguments) {
-    JCommander parser = JCommander.newBuilder()
-      .addObject(this.main)
-      .build();
-    parser.parse(arguments.toArray(new String[0]));
-    return this.main.toExecutableCommand();
+    JCommander jCommander = this.jCommanderBuilder.build();
+    jCommander.parse(arguments.toArray(new String[0]));
+
+    String selectedCommand = jCommander.getParsedCommand();
+    JCommanderArguments selectedArgs = this.commandArgs.getOrDefault(selectedCommand, this.main);
+    return selectedArgs.toExecutableCommand();
   }
 
   public interface JCommanderArguments {
--- lambdas-and-strings/console-runner/src/test/java/info/javaspec/console/MultiCommandParserTest.java
+++ lambdas-and-strings/console-runner/src/test/java/info/javaspec/console/MultiCommandParserTest.java
@@ -6,10 +6,12 @@ import de.bechte.junit.runners.context.HierarchicalContextRunner;
 import info.javaspec.console.MultiCommandParser.JCommanderArguments;
 import org.hamcrest.Matchers;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -17,37 +19,65 @@ import static org.hamcrest.MatcherAssert.assertThat;
 @RunWith(HierarchicalContextRunner.class)
 public class MultiCommandParserTest {
   public class parseCommand {
-    private Main.ArgumentParser subject;
-    private Command mainReturns = Mockito.mock(Command.class);
+    private MultiCommandParser subject;
+    private Command mainExecutableCommand = Mockito.mock(Command.class);
 
-    public class givenArgumentsThatDoNotMatchAnyCommand {
+    public class givenInvalidParametersForTheMainCommand {
       @Test(expected = ParameterException.class)
       public void throwsAnException() throws Exception {
-        JCommanderArguments main = Mockito.mock(JCommanderArguments.class);
-        subject = new MultiCommandParser(main);
+        JCommanderArguments mainArgsWithNoOptions = () -> mainExecutableCommand;
+        subject = new MultiCommandParser(mainArgsWithNoOptions);
         subject.parseCommand(Collections.singletonList("--unknown-option"));
       }
     }
 
-    public class givenOptionsMatchingTheMainCommand {
+    public class givenACommandWithInvalidParameters {
+      @Test(expected = ParameterException.class)
+      public void throwsAnException() throws Exception {
+        JCommanderArguments mainArgsWithNoOptions = () -> mainExecutableCommand;
+        subject = new MultiCommandParser(mainArgsWithNoOptions);
+        subject.addCliCommand("valid-command", () -> Mockito.mock(Command.class));
+        subject.parseCommand(Arrays.asList("valid-command", "--unknown-option"));
+      }
+    }
+
+    public class givenOptionsMatchingTheMainCliCommand {
       @Before
       public void setup() throws Exception {
-        JCommanderArguments mainWithValidOption = new JCommanderArguments() {
+        JCommanderArguments mainArgsWithValidOption = new JCommanderArguments() {
           @Parameter(names = "--valid-option")
           public boolean validOption;
 
           @Override
           public Command toExecutableCommand() {
-            return mainReturns;
+            return mainExecutableCommand;
           }
         };
-        subject = new MultiCommandParser(mainWithValidOption);
+        subject = new MultiCommandParser(mainArgsWithValidOption);
       }
 
       @Test
       public void parsesAllArgumentsAsMainArguments() throws Exception {
         Command returned = subject.parseCommand(Collections.singletonList("--valid-option"));
-        assertThat(returned, Matchers.sameInstance(mainReturns));
+        assertThat(returned, Matchers.sameInstance(mainExecutableCommand));
+      }
+    }
+
+    public class givenArgumentsMatchingACliCommand {
+      private Command oneExecutableCommand = Mockito.mock(Command.class);
+
+      @Before
+      public void setup() throws Exception {
+        JCommanderArguments main = () -> mainExecutableCommand;
+        JCommanderArguments one = () -> oneExecutableCommand;
+        subject = new MultiCommandParser(main);
+        subject.addCliCommand("do-one-thing", one);
+      }
+
+      @Test
+      public void parsesThatCommandAndItsArguments() throws Exception {
+        Command returned = subject.parseCommand(Collections.singletonList("do-one-thing"));
+        assertThat(returned, Matchers.sameInstance(oneExecutableCommand));
       }
     }
   }
