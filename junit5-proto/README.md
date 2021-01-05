073f9a72f4d63d35f7dcb34ade9343bd87a8a224
Kyle Krull <kkrull@8thlight.com>
2021-01-05 09:11:04 -0600

    Learn how to show formatted output for JUnit-jupter tests

--- junit5-proto/README.md
+++ junit5-proto/README.md
@@ -1,3 +1,8 @@
 # JUnit5 Prototype for JavaSpec
 
 TODO KDK: See if the dang thing builds.
+
+## IntelliJ IDEA
+
+Set the test runner in (Preferences | Build, Execution, Deployment | Build Tools | Gradle) to "IntelliJ IDEA", instead of the default "Gradle",
+in order to show `@DisplayName` properly, from JUnit Jupiter tests.
--- junit5-proto/lib/src/test/java/info/javaspec/jupiter/LibraryTest.java
+++ junit5-proto/lib/src/test/java/info/javaspec/jupiter/LibraryTest.java
@@ -1,10 +1,13 @@
 package info.javaspec.jupiter;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@DisplayName("Library")
 class LibraryTest {
+  @DisplayName("returns true")
   @Test void testSomeLibraryMethod() {
     Library classUnderTest = new Library();
     assertTrue(classUnderTest.someLibraryMethod(), "someLibraryMethod should return 'true'");
