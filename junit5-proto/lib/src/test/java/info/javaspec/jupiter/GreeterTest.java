5cc6407f9fb1cb448add599748a5cc9b9ec65332
Kyle Krull <kkrull@8thlight.com>
2021-01-05 22:36:11 -0600

    Extract object JupiterSpec

--- junit5-proto/lib/src/test/java/info/javaspec/jupiter/GreeterTest.java
+++ junit5-proto/lib/src/test/java/info/javaspec/jupiter/GreeterTest.java
@@ -2,15 +2,32 @@ package info.javaspec.jupiter;
 
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
+import org.junit.jupiter.api.function.Executable;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GreeterTest {
   @TestFactory
   DynamicTest makeSingleTest() {
-    return DynamicTest.dynamicTest("greets the world", () -> {
+    JupiterSpec spec = new JupiterSpec("greets the world", () -> {
       Greeter subject = new Greeter();
       assertEquals("Hello world!", subject.makeGreeting());
     });
+
+    return spec.toDynamicTest();
+  }
+
+  static class JupiterSpec {
+    private final String behavior;
+    private final Executable verification;
+
+    public JupiterSpec(String behavior, Executable verification) {
+      this.behavior = behavior;
+      this.verification = verification;
+    }
+
+    public DynamicTest toDynamicTest() {
+      return DynamicTest.dynamicTest(behavior, verification);
+    }
   }
 }
