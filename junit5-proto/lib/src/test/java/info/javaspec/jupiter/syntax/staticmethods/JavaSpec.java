7e05151d070a7b7046ddb75c3c90eb44dcd1e10a
Kyle Krull <kkrull@8thlight.com>
2021-01-11 16:10:32 -0600

    Add prototype for a single describe+it

--- junit5-proto/lib/src/test/java/info/javaspec/jupiter/syntax/staticmethods/JavaSpec.java
+++ junit5-proto/lib/src/test/java/info/javaspec/jupiter/syntax/staticmethods/JavaSpec.java
@@ -1,11 +1,31 @@
 package info.javaspec.jupiter.syntax.staticmethods;
 
+import org.junit.jupiter.api.DynamicContainer;
 import org.junit.jupiter.api.DynamicNode;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.function.Executable;
 
+import java.util.LinkedList;
+import java.util.List;
+
 final class JavaSpec {
-  public static DynamicNode it(String behavior, Executable verification) {
-    return DynamicTest.dynamicTest(behavior, verification);
+  private static List<DynamicTest> currentTests;
+
+  public static DynamicNode describe(String actor, DescribeBlock block) {
+    currentTests = new LinkedList<>();
+    block.declare();
+
+    DynamicContainer container = DynamicContainer.dynamicContainer(actor, currentTests);
+    currentTests = null;
+    return container;
+  }
+
+  public static void it(String behavior, Executable verification) {
+    currentTests.add(DynamicTest.dynamicTest(behavior, verification));
+  }
+
+  @FunctionalInterface
+  public interface DescribeBlock {
+    void declare();
   }
 }
--- junit5-proto/lib/src/test/java/info/javaspec/jupiter/syntax/staticmethods/StaticMethodSyntax.java
+++ junit5-proto/lib/src/test/java/info/javaspec/jupiter/syntax/staticmethods/StaticMethodSyntax.java
@@ -1,16 +1,30 @@
 package info.javaspec.jupiter.syntax.staticmethods;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import info.javaspec.jupiter.Greeter;
 import org.junit.jupiter.api.DynamicNode;
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
+
+import static info.javaspec.jupiter.syntax.staticmethods.JavaSpec.describe;
+import static info.javaspec.jupiter.syntax.staticmethods.JavaSpec.it;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 //Shows how to use static imports for spec syntax.
 //TODO KDK: Identify pros and cons, in relation to the other syntax prototypes.
 class StaticMethodSyntax {
   @TestFactory
-  DynamicNode generateTests() {
-    //TODO KDK: Work here to static import JavaSpec methods
-    return JavaSpec.it("asserts", () -> assertEquals(2, 1+1));
+  DynamicNode generateOneDescribe() {
+    return describe("Greeter", () -> {
+      it("greets the world", () -> {
+        Greeter subject = new Greeter();
+        assertEquals("Hello world!", subject.makeGreeting());
+      });
+    });
+  }
+
+  @TestFactory
+  DynamicNode generateOneDescribeMultipleIt() {
+    //TODO KDK: Generate nested tests to drive a stack structure.
+    return DynamicTest.dynamicTest("placeholder", () -> assertEquals(2, 1+1));
   }
 }
