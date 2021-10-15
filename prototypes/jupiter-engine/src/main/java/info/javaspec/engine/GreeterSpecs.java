73d905d9adec48d1c2167eff46be74b026b1e9d0
Kyle Krull <kkrull@8thlight.com>
2021-10-15 15:25:40 -0500

    Stub in Main to run a Launcher manually

---
+++ prototypes/jupiter-engine/src/main/java/info/javaspec/engine/GreeterSpecs.java
@@ -0,0 +1,15 @@
+package info.javaspec.engine;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GreeterSpecs {
+  private static int _numTimesRun = 0;
+
+  public static void assertRanOnce() {
+    assertEquals(1, _numTimesRun, "Expected GreeterSpecs to have been run once");
+  }
+
+  public GreeterSpecs() {
+    System.out.println("[GreeterSpecs::GreeterSpecs]");
+  }
+}
--- prototypes/jupiter-engine/src/main/java/info/javaspec/engine/Main.java
+++ prototypes/jupiter-engine/src/main/java/info/javaspec/engine/Main.java
@@ -1,4 +1,83 @@
 package info.javaspec.engine;
 
+import org.junit.jupiter.engine.JupiterTestEngine;
+import org.junit.platform.engine.*;
+import org.junit.platform.launcher.*;
+import org.junit.platform.launcher.core.LauncherConfig;
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
+import org.junit.platform.launcher.core.LauncherFactory;
+
+import static org.junit.platform.engine.discovery.DiscoverySelectors.selectClass;
+
 public class Main {
+  public static void main(String[] args) {
+    System.out.println("Main says hello!");
+
+    LauncherConfig launcherConfig = LauncherConfig.builder()
+      .enableTestEngineAutoRegistration(false)
+      .enableTestExecutionListenerAutoRegistration(false)
+      .addTestEngines(new JupiterTestEngine())
+//      .addTestEngines(new JavaSpecTestEngine())
+      .addLauncherDiscoveryListeners(launcherDiscoveryListener())
+      .addTestExecutionListeners(testExecutionListener())
+      .build();
+
+    Launcher launcher = LauncherFactory.create(launcherConfig);
+    launcher.execute(discoverRequestForTestClass());
+    GreeterSpecs.assertRanOnce();
+  }
+
+  private static LauncherDiscoveryRequest discoverRequestForTestClass() {
+    return LauncherDiscoveryRequestBuilder.request()
+      .selectors(selectClass(GreeterSpecs.class))
+      .build();
+  }
+
+  private static LauncherDiscoveryListener launcherDiscoveryListener() {
+    return new LauncherDiscoveryListener() {
+      @Override
+      public void launcherDiscoveryStarted(LauncherDiscoveryRequest request) {
+        System.out.println("[launcherDiscoveryStarted]");
+      }
+
+      @Override
+      public void engineDiscoveryStarted(UniqueId engineId) {
+        System.out.println("[engineDiscoveryStarted] %s".formatted(engineId));
+      }
+
+      @Override
+      public void engineDiscoveryFinished(UniqueId engineId, EngineDiscoveryResult result) {
+        System.out.println("[engineDiscoveryFinished] %s: %s".formatted(engineId, result.getStatus()));
+      }
+
+      @Override
+      public void launcherDiscoveryFinished(LauncherDiscoveryRequest request) {
+        System.out.println("[launcherDiscoveryFinished]");
+      }
+    };
+  }
+
+  private static TestExecutionListener testExecutionListener() {
+    return new TestExecutionListener() {
+      @Override
+      public void testPlanExecutionStarted(TestPlan testPlan) {
+        System.out.println("[testPlanExecutionStarted]");
+      }
+
+      @Override
+      public void executionStarted(TestIdentifier testId) {
+        System.out.println("[executionStarted] %s (%s)".formatted(testId.getUniqueId(), testId.getDisplayName()));
+      }
+
+      @Override
+      public void executionFinished(TestIdentifier testId, TestExecutionResult testExecutionResult) {
+        System.out.println("[executionFinished] %s (%s)".formatted(testId.getUniqueId(), testId.getDisplayName()));
+      }
+
+      @Override
+      public void testPlanExecutionFinished(TestPlan testPlan) {
+        System.out.println("[testPlanExecutionFinished]");
+      }
+    };
+  }
 }
