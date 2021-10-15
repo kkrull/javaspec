6a623bb3c547e352f4135647d79f2d1dc32f31b7
Kyle Krull <kkrull@8thlight.com>
2021-10-15 15:43:24 -0500

    Add SpecDescriptor

--- prototypes/jupiter-engine/src/main/java/info/javaspec/engine/SpecTestEngine.java
+++ prototypes/jupiter-engine/src/main/java/info/javaspec/engine/SpecTestEngine.java
@@ -1,11 +1,16 @@
 package info.javaspec.engine;
 
 import org.junit.platform.engine.*;
+import org.junit.platform.engine.support.descriptor.AbstractTestDescriptor;
+import org.junit.platform.engine.support.descriptor.EngineDescriptor;
 
 public class SpecTestEngine implements TestEngine {
   @Override
-  public TestDescriptor discover(EngineDiscoveryRequest discoveryRequest, UniqueId uniqueId) {
-    throw new UnsupportedOperationException();
+  public TestDescriptor discover(EngineDiscoveryRequest discoveryRequest, UniqueId engineId) {
+    EngineDescriptor engineDescriptor = new EngineDescriptor(engineId, "JavaSpec");
+    TestDescriptor specDescriptor = SpecDescriptor.forSpec(engineId, "greets the world");
+    engineDescriptor.addChild(specDescriptor);
+    return engineDescriptor;
   }
 
   @Override
@@ -16,5 +21,23 @@ public class SpecTestEngine implements TestEngine {
   @Override
   public String getId() {
     return "javaspec-engine";
+  }
+
+  private static class SpecDescriptor extends AbstractTestDescriptor {
+    public static SpecDescriptor forSpec(UniqueId engineId, String behavior) {
+      return new SpecDescriptor(
+        engineId.append("spec", "lambda %s".formatted(behavior)),
+        behavior
+      );
+    }
+
+    private SpecDescriptor(UniqueId uniqueId, String displayName) {
+      super(uniqueId, displayName);
+    }
+
+    @Override
+    public Type getType() {
+      return Type.TEST;
+    }
   }
 }
