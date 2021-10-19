0297538a4928b21ca2660807dd3d099d116dca48
Kyle Krull <kkrull@8thlight.com>
2021-10-19 09:10:06 -0500

    Add intermediate test descriptor for spec class

--- prototypes/jupiter-engine/src/main/java/info/javaspec/engine/JupiterSpecContainer.java
+++ prototypes/jupiter-engine/src/main/java/info/javaspec/engine/JupiterSpecContainer.java
@@ -1,6 +1,8 @@
 package info.javaspec.engine;
 
 import org.junit.platform.engine.TestDescriptor;
+import org.junit.platform.engine.UniqueId;
+import org.junit.platform.engine.support.descriptor.AbstractTestDescriptor;
 
 public class JupiterSpecContainer {
   private JupiterSpec spec;
@@ -10,6 +12,30 @@ public class JupiterSpecContainer {
   }
 
   public void addDescriptorsTo(TestDescriptor parentDescriptor) {
-    spec.addTestDescriptorTo(parentDescriptor);
+    ContainerDescriptor containerDescriptor = ContainerDescriptor.forClass(
+      parentDescriptor.getUniqueId(),
+      GreeterSpecs.class //TODO KDK: Lift parameter to constructor
+    );
+    parentDescriptor.addChild(containerDescriptor);
+
+    spec.addTestDescriptorTo(containerDescriptor);
+  }
+
+  private static class ContainerDescriptor extends AbstractTestDescriptor {
+    public static ContainerDescriptor forClass(UniqueId parentId, Class<?> specClass) {
+      return new ContainerDescriptor(
+        parentId.append("class", specClass.getName()),
+        specClass.getName()
+      );
+    }
+
+    private ContainerDescriptor(UniqueId uniqueId, String displayName) {
+      super(uniqueId, displayName);
+    }
+
+    @Override
+    public Type getType() {
+      return Type.CONTAINER;
+    }
   }
 }
