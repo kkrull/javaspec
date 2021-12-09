d1af5cbe96d9b09e8de3421b813a509f80ed1d8d
Kyle Krull <kkrull@8thlight.com>
2021-12-09 15:35:43 -0600

    Identify next steps

--- prototypes/javaspec-client/src/test/java/info/javaspec/client/GreeterSpecs.java
+++ prototypes/javaspec-client/src/test/java/info/javaspec/client/GreeterSpecs.java
@@ -3,6 +3,7 @@ package info.javaspec.client;
 import info.javaspec.api.SpecClass;
 import info.javaspec.api.SpecContainer;
 
+//TODO KDK: [2] Create a separate javaspec-launcher module that is hard-coded to run these specs, then start extracting an engine and making it dynamic.
 public class GreeterSpecs implements SpecClass {
   @Override
   public void declareSpecs(SpecContainer container) {
