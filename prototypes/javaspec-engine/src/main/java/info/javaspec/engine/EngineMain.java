5e42f13f902c30c98d7de6d22ebec2b64f2cb2c1
Kyle Krull <kkrull@8thlight.com>
2021-12-09 17:08:55 -0600

    Promote javaspec-engine to an application for now
    so that it can run itself from a mainClass

---
+++ prototypes/javaspec-engine/src/main/java/info/javaspec/engine/EngineMain.java
@@ -0,0 +1,8 @@
+package info.javaspec.engine;
+
+public class EngineMain {
+  //TODO KDK: [0] Run with ./gradlew javaspec-engine:run
+  public static void main(String[] args) {
+    throw new UnsupportedOperationException("Work here: Create a TestLauncher that uses this engine");
+  }
+}
