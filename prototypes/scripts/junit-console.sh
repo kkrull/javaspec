9e48073bc520f515aa319be7b8418b35dd40c437
Kyle Krull <kkrull@8thlight.com>
2022-02-03 09:05:16 -0600

    Update script to run the engine

--- prototypes/scripts/junit-console.sh
+++ prototypes/scripts/junit-console.sh
@@ -2,9 +2,19 @@
 
 set -e
 
-junit_console_jar='/usr/local/opt/java/junit-platform-console-standalone-1.8.1.jar'
+scripts_dir=$(greadlink -f "$(dirname -- "${BASH_SOURCE[0]}")")
+project_dir=$(greadlink -f "$scripts_dir/..")
+
+lib_dir="$project_dir/lib"
+junit_console_jar="$lib_dir/junit-platform-console-standalone-1.8.1.jar"
+
+#Make sure engine jar is up to date: ./gradlew :javaspec-engine:assemble && ./scripts/junit-console.sh
+cd "$project_dir"
 java -jar "$junit_console_jar" \
   --classpath=javaspec-api/build/classes/java/main \
+  --classpath=javaspec-client/build/classes/java/main \
   --classpath=javaspec-client/build/classes/java/test \
+  --classpath=javaspec-engine/build/libs/javaspec-engine-0.0.1.jar \
+  --include-engine=javaspec-engine \
   --select-class=info.javaspec.client.GreeterSpecs \
   $@
