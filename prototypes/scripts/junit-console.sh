de213024cf2bf897cb18022c2d0024ed1ddccfe9
Kyle Krull <kkrull@8thlight.com>
2022-02-03 15:12:19 -0600

    Stop defaulting to that one spec class

--- prototypes/scripts/junit-console.sh
+++ prototypes/scripts/junit-console.sh
@@ -9,6 +9,7 @@ lib_dir="$project_dir/lib"
 junit_console_jar="$lib_dir/junit-platform-console-standalone-1.8.1.jar"
 
 #Make sure engine jar is up to date: ./gradlew :javaspec-engine:assemble && ./scripts/junit-console.sh
+#Remember --select-class=info.javaspec.client.GreeterSpecs, too (or it won't discover any specs)
 cd "$project_dir"
 java -jar "$junit_console_jar" \
   --classpath=javaspec-api/build/classes/java/main \
@@ -16,5 +17,4 @@ java -jar "$junit_console_jar" \
   --classpath=javaspec-client/build/classes/java/test \
   --classpath=javaspec-engine/build/libs/javaspec-engine-0.0.1.jar \
   --include-engine=javaspec-engine \
-  --select-class=info.javaspec.client.GreeterSpecs \
   $@
