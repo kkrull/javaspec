ab0bc94b96cf0b266708ebd7e0ffcfdf089cc449
Kyle Krull <kkrull@8thlight.com>
2022-02-03 09:33:48 -0600

    Confirm that IntelliJ runs the selected class
    when running a @Testable spec class

---
+++ prototypes/javaspec-client/src/test/java/info/javaspec/client/OtherSpecs.java
@@ -0,0 +1,26 @@
+package info.javaspec.client;
+
+import info.javaspec.api.JavaSpec;
+import info.javaspec.api.SpecClass;
+import org.junit.platform.commons.annotation.Testable;
+
+@Testable
+public class OtherSpecs implements SpecClass {
+  public void declareSpecs(JavaSpec javaspec) {
+    javaspec.it("answers the meaning of life", () -> {
+      int answer = 42;
+      assertEquals(42, answer);
+    });
+  }
+
+  private static void assertEquals(int expected, int actual) {
+    if(actual == expected)
+      return;
+
+    throw new AssertionError(String.format(
+      "Expected <%s>, but was <%s>",
+      expected,
+      actual
+    ));
+  }
+}
