60d13771e18cdd61ef7607a6b7f6c7e6b7f022fe
Kyle Krull <kkrull@8thlight.com>
2022-02-04 16:52:08 -0600

    Add failing test for an empty spec class
    Next action: Implement SpecClass so that the engine can at least load
    the class.

---
+++ prototypes/javaspec-engine/src/test/java/info/javaspec/engine/EmptySpecs.java
@@ -0,0 +1,4 @@
+package info.javaspec.engine;
+
+//An empty class that has no specs in it.  Guess how many tests it should run?
+public class EmptySpecs { /* empty */ }
--- prototypes/javaspec-engine/src/test/java/info/javaspec/engine/JavaSpecEngineTest.java
+++ prototypes/javaspec-engine/src/test/java/info/javaspec/engine/JavaSpecEngineTest.java
@@ -1,13 +1,27 @@
 package info.javaspec.engine;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.testkit.engine.EngineTestKit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.platform.engine.discovery.DiscoverySelectors.selectClass;
 
+@DisplayName("JavaSpecEngine")
 public class JavaSpecEngineTest {
   @Test
-  public void sayHello() throws Exception {
-    String greeting = "Hello world!";
-    assertEquals("Hello world!", greeting);
+  @DisplayName("loads EngineTestKit like a boss")
+  public void loadsEngineTestKit() throws Exception {
+    EngineTestKit.engine("javaspec-engine");
+  }
+
+  @Test
+  @DisplayName("complies with the Jupiter Core API for TestEngines")
+  public void compliesWithJupiterCore() throws Exception {
+    EngineTestKit.engine("javaspec-engine")
+      .selectors(selectClass(EmptySpecs.class))
+      .execute()
+      .containerEvents()
+      .assertStatistics(stats -> stats.started(0).finished(0));
   }
 }
