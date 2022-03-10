e7336a5212c425e07050e471ae72a814a3823dca
Kyle Krull <kkrull@8thlight.com>
2022-03-10 14:27:23 -0600

    Start to act like a TestEngine

--- prototypes/javaspec-engine-v2/src/main/java/info/javaspec/engine/JavaSpecEngine.java
+++ prototypes/javaspec-engine-v2/src/main/java/info/javaspec/engine/JavaSpecEngine.java
@@ -1,4 +1,19 @@
 package info.javaspec.engine;
 
-public class JavaSpecEngine {
+import org.junit.platform.engine.*;
+
+public class JavaSpecEngine implements TestEngine {
+	@Override
+	public TestDescriptor discover(EngineDiscoveryRequest discoveryRequest, UniqueId uniqueId) {
+		return null;
+	}
+
+	@Override
+	public void execute(ExecutionRequest request) {
+	}
+
+	@Override
+	public String getId() {
+		return "javaspec-engine-v2";
+	}
 }
--- prototypes/javaspec-engine-v2/src/test/java/info/javaspec/engine/JavaSpecEngineTest.java
+++ prototypes/javaspec-engine-v2/src/test/java/info/javaspec/engine/JavaSpecEngineTest.java
@@ -1,9 +1,13 @@
 package info.javaspec.engine;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.engine.TestEngine;
 import org.junit.platform.testkit.engine.EngineTestKit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Nested
 public class JavaSpecEngineTest {
@@ -14,8 +18,22 @@ public class JavaSpecEngineTest {
 	}
 
 	@Test
+	@DisplayName("returns an unique ID")
+	public void identifiesItselfAsTheEngineForJavaSpec() throws Exception {
+	  TestEngine subject = new JavaSpecEngine();
+		assertEquals("javaspec-engine-v2", subject.getId());
+	}
+
+	@Test
 	@DisplayName("runs as a TestEngine")
+	@Disabled
 	public void runsAsATestEngine () throws Exception {
-		EngineTestKit.engine("javaspec-engine-v2").execute();
+		EngineTestKit.engine(new JavaSpecEngine()).execute();
+	}
+
+	@Test
+	@DisplayName("can be loaded with ServiceLoader and located by ID")
+	@Disabled
+	public void isRegisteredWithServiceLoader() throws Exception {
 	}
 }
