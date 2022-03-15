1ffd6f9de8e128d7f767019f1a7331eb7d948dac
Kyle Krull <kkrull@8thlight.com>
2022-03-15 10:00:48 -0500

    Apply format to existing projects

--- prototypes/javaspec-engine-discovery-request-listener/src/main/java/info/javaspec/engine/ConsoleEngineDiscoveryRequestListener.java
+++ prototypes/javaspec-engine-discovery-request-listener/src/main/java/info/javaspec/engine/ConsoleEngineDiscoveryRequestListener.java
@@ -11,47 +11,47 @@ public class ConsoleEngineDiscoveryRequestListener implements EngineDiscoveryReq
 		System.out.println();
 		System.out.printf("[ConsoleEngineDiscoveryRequestListener] ClasspathResourceSelector%n");
 		discoveryRequest.getSelectorsByType(ClasspathResourceSelector.class)
-				.forEach(x -> System.out.printf("- %s%n", x.getClasspathResourceName()));
+			.forEach(x -> System.out.printf("- %s%n", x.getClasspathResourceName()));
 
 		System.out.println();
 		System.out.printf("[ConsoleEngineDiscoveryRequestListener] ClasspathRootSelector%n");
 		discoveryRequest.getSelectorsByType(ClasspathRootSelector.class)
-				.forEach(x -> System.out.printf("- %s%n", x.getClasspathRoot()));
+			.forEach(x -> System.out.printf("- %s%n", x.getClasspathRoot()));
 
 		System.out.println();
 		System.out.printf("[ConsoleEngineDiscoveryRequestListener] ClassSelector%n");
 		discoveryRequest.getSelectorsByType(ClassSelector.class)
-				.forEach(x -> System.out.printf("- %s%n", x.getClassName()));
+			.forEach(x -> System.out.printf("- %s%n", x.getClassName()));
 
 		System.out.println();
 		System.out.printf("[ConsoleEngineDiscoveryRequestListener] DirectorySelector%n");
 		discoveryRequest.getSelectorsByType(DirectorySelector.class)
-				.forEach(x -> System.out.printf("- %s%n", x.getRawPath()));
+			.forEach(x -> System.out.printf("- %s%n", x.getRawPath()));
 
 		System.out.println();
 		System.out.printf("[ConsoleEngineDiscoveryRequestListener] FileSelector%n");
 		discoveryRequest.getSelectorsByType(FileSelector.class)
-				.forEach(x -> System.out.printf("- %s%n", x.getRawPath()));
+			.forEach(x -> System.out.printf("- %s%n", x.getRawPath()));
 
 		System.out.println();
 		System.out.printf("[ConsoleEngineDiscoveryRequestListener] MethodSelector%n");
 		discoveryRequest.getSelectorsByType(MethodSelector.class)
-				.forEach(x -> System.out.printf("- %s#%s%n", x.getClassName(), x.getMethodName()));
+			.forEach(x -> System.out.printf("- %s#%s%n", x.getClassName(), x.getMethodName()));
 
 		System.out.println();
 		System.out.printf("[ConsoleEngineDiscoveryRequestListener] PackageSelector%n");
 		discoveryRequest.getSelectorsByType(PackageSelector.class)
-				.forEach(x -> System.out.printf("- %s%n", x.getPackageName()));
+			.forEach(x -> System.out.printf("- %s%n", x.getPackageName()));
 
 		System.out.println();
 		System.out.printf("[ConsoleEngineDiscoveryRequestListener] UniqueIdSelector%n");
 		discoveryRequest.getSelectorsByType(UniqueIdSelector.class)
-				.forEach(x -> System.out.printf("- %s%n", x.getUniqueId()));
+			.forEach(x -> System.out.printf("- %s%n", x.getUniqueId()));
 
 		System.out.println();
 		System.out.printf("[ConsoleEngineDiscoveryRequestListener] UriSelector%n");
 		discoveryRequest.getSelectorsByType(UriSelector.class)
-				.forEach(x -> System.out.printf("- %s%n", x.getUri().getRawPath()));
+			.forEach(x -> System.out.printf("- %s%n", x.getUri().getRawPath()));
 
 		System.out.println();
 		System.out.printf("[ConsoleEngineDiscoveryRequestListener] ClassNameFilter%n");
