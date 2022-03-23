9ef5b2dd561eeabed1751ae865522470211fdedc
Kyle Krull <kkrull@8thlight.com>
2022-03-23 09:19:42 -0500

    Clarify use of Composite pattern

--- prototypes/javaspec-engine/src/main/java/info/javaspec/engine/ExecutableTestDescriptor.java
+++ prototypes/javaspec-engine/src/main/java/info/javaspec/engine/ExecutableTestDescriptor.java
@@ -3,7 +3,7 @@ package info.javaspec.engine;
 import org.junit.platform.engine.EngineExecutionListener;
 import org.junit.platform.engine.TestDescriptor;
 
-//Adapter for JavaSpec containers or tests that runs on Jupiter.
+//Composite adapter for JavaSpec containers or tests that runs on Jupiter and only contains other executable descriptors.
 interface ExecutableTestDescriptor extends TestDescriptor {
 	void execute(EngineExecutionListener listener);
 }
