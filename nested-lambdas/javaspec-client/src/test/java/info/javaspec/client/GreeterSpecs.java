1eca1e49b814774f629e4496a079179e127f59ff
Kyle Krull <kkrull@8thlight.com>
2022-03-30 09:54:48 -0500

    Refactor other client specs to use more JavaSpec features

--- nested-lambdas/javaspec-client/src/test/java/info/javaspec/client/GreeterSpecs.java
+++ nested-lambdas/javaspec-client/src/test/java/info/javaspec/client/GreeterSpecs.java
@@ -10,30 +10,15 @@ import org.junit.platform.commons.annotation.Testable;
 //Without @Testable, you can still pick "Run Tests" on a package/directory.
 @Testable
 public class GreeterSpecs implements SpecClass {
-	private static int _numTimesRun = 0;
-
-	// Called via reflection
-	public static void assertRanOnce() {
-		assertEquals(1, _numTimesRun, "Expected GreeterSpecs to have been run once");
-	}
-
-	public static void incrementRunCount() {
-		_numTimesRun++;
-	}
-
 	public void declareSpecs(JavaSpec javaspec) {
-		javaspec.it("greets the world", () -> {
-			incrementRunCount();
-			Greeter subject = new Greeter();
-			assertEquals("Hello world!", subject.greet());
+		javaspec.describe(Greeter.class, () -> {
+			javaspec.describe("#greet", () -> {
+				javaspec.it("greets the world", () -> {
+					Greeter subject = new Greeter();
+					assertEquals("Hello world!", subject.greet());
+				});
+			});
 		});
-	}
-
-	private static void assertEquals(int expected, int actual, String message) {
-		if (actual == expected)
-			return;
-
-		throw new AssertionError(String.format("%s: Expected <%s>, but was <%s>", message, expected, actual));
 	}
 
 	private static void assertEquals(String expected, String actual) {
