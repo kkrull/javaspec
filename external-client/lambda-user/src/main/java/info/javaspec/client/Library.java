70f67d1fccadd278ba977cf1e45320f23d2f8a0c
Kyle Krull <kdkrull@gmail.com>
2022-04-09 14:58:52 -0500

    Apply editor and format settings

--- external-client/lambda-user/src/main/java/info/javaspec/client/Library.java
+++ external-client/lambda-user/src/main/java/info/javaspec/client/Library.java
@@ -4,7 +4,7 @@
 package info.javaspec.client;
 
 public class Library {
-    public boolean someLibraryMethod() {
-        return true;
-    }
+	public boolean someLibraryMethod() {
+		return true;
+	}
 }
