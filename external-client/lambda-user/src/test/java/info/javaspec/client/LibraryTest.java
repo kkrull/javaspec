961f992037c903997186699453ba5cf4c4cac4c9
Kyle Krull <kkrull@8thlight.com>
2022-04-11 16:58:34 -0500

    Implement a spec but not run it yet

--- external-client/lambda-user/src/test/java/info/javaspec/client/LibraryTest.java
+++ external-client/lambda-user/src/test/java/info/javaspec/client/LibraryTest.java
@@ -2,12 +2,26 @@ package info.javaspec.client;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import info.javaspec.api.JavaSpec;
+import info.javaspec.api.SpecClass;
 import org.junit.jupiter.api.Test;
 
-class LibraryTest {
+class LibraryTest implements SpecClass {
 	@Test
 	void someLibraryMethodReturnsTrue() {
 		Library subject = new Library();
 		assertTrue(subject.someLibraryMethod(), "someLibraryMethod should return 'true'");
+	}
+
+	@Override
+	public void declareSpecs(JavaSpec javaspec) {
+		javaspec.describe(Library.class, () -> {
+			javaspec.describe("#someLibraryMethod", () -> {
+				javaspec.it("returns true", () -> {
+					Library subject = new Library();
+					assertTrue(subject.someLibraryMethod());
+				});
+			});
+		});
 	}
 }
