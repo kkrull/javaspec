4d3e46ba0d8447dc1bc11bcd358185b7937b46d3
Kyle Krull <kkrull@8thlight.com>
2022-04-11 17:24:34 -0500

    Complete conversion to JavaSpec

--- external-client/lambda-user/src/test/java/info/javaspec/client/LibraryTest.java
+++ external-client/lambda-user/src/test/java/info/javaspec/client/LibraryTest.java
@@ -7,19 +7,13 @@ import info.javaspec.api.SpecClass;
 import org.junit.jupiter.api.Test;
 
 public class LibraryTest implements SpecClass {
-	@Test
-	void someLibraryMethodReturnsTrue() {
-		Library subject = new Library();
-		assertTrue(subject.someLibraryMethod(), "someLibraryMethod should return 'true'");
-	}
-
 	@Override
 	public void declareSpecs(JavaSpec javaspec) {
 		javaspec.describe(Library.class, () -> {
 			javaspec.describe("#someLibraryMethod", () -> {
 				javaspec.it("returns true", () -> {
 					Library subject = new Library();
-					assertFalse(subject.someLibraryMethod());
+					assertTrue(subject.someLibraryMethod());
 				});
 			});
 		});
